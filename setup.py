0b181ce51f5b8c1584b89bec05f5ebf047880b6b
Amine <amine@stanford.edu>
2024-06-22 15:01:00 -0700

    adjusted setup.py compiler args to fix invalid memory access error

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
                 "cuda_rasterizer/forward.cu",
                 "cuda_rasterizer/backward.cu",
                 "rasterize_points.cu",
-                "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                             "third_party/glm/")]})
+                "ext.cpp"
+            ],
+            extra_compile_args={"nvcc": ["-Xcompiler", "-fno-gnu-unique","-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]}
     ],
     cmdclass={
         'build_ext': BuildExtension
